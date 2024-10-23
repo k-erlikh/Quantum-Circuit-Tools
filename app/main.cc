ce743dbf334a824784e2ed55fbb8ba1baa249614
k-erlikh <kateerlikh@cmail.carleton.ca>
2024-10-23 12:39:01 -0400

    handles -q flag

--- app/main.cc
+++ app/main.cc
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "Matrix.h"
+#include "Flags.h"
+#include "Circuit.h"
 
 int main(int argc, char * argv[]){
-    Matrix<double> matrix(3,3);
-    matrix.print_matrix();
+    Circuit circuit(0,0);
+    for(int i = 0; i < argc-1; i+2){
+       if(argv[i] == "-q"){
+            Flags::handle_q(circuit, argv[i+1]);
+       }
+    }
     return 0;
 }
