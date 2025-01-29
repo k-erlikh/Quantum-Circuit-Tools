33e92d45ee7b725c1efe8e7171cb59cd9eee05c6
k-erlikh <kateerlikh@cmail.carleton.ca>
2025-01-29 17:06:54 -0500

    Fixed how flags will be called. Instead, user wil sepcify amount of quibts, gates on which qubits, and pick to simulate specific states (similar to MATLAB qunatum library)

--- app/main.cc
+++ app/main.cc
@@ -9,28 +9,30 @@ namespace bpo = boost::program_options;
 int main(int argc, char * argv[]){
     Circuit circuit(0,0);
 
-    bpo::options_description desc("Allowed options");
+    bpo::options_description desc("Flag options");
     desc.add_options()
         ("help", R"""(
         General help: 
         1. What are the possible qubit states? 
-        Possible qubit configurations: 0 = |0⟩,  1 = |1⟩,  2 = |+⟩, 3 = |−⟩
+        Possible qubit configurations: 0 = |0⟩,  1 = |1⟩,  + = |+⟩, - = |−⟩
 
         2. What order should I add the flags?
-        In sequential order
+            a. Numebr of qubits (-q)
+            b. Gates 
+            c. Simulate 
 
         )""")
-        ("qubit,q", bpo::value<char>(), "Set starting qubit state. See -help for possible states.")
-        ("paulix,px", "Add paulix gate to most recently added qubit")
-        ("control,c", bpo::value<std::vector<char>>()->multitoken(), "Add control between most recently added gate and qubit");
+        ("qubit,q", bpo::value<int>(), "Set number of qubits in circuit.")
+        ("hadamard,h", bpo::value<string>(),"Add Hadamard gate to qubit in this format: <qubit>:<qubit>. If not connecting gate to anything, enter second qubit value as zero. (ex: 1:0)")
+        ("paulix,px", bpo::value<string>(),"Add paulix gate to qubit in this format: <qubit>:<qubit>. If not connecting gate to anything, enter second qubit value as zero. (ex: 1:0)")
+        ("simulate,s", bpo::value<int>(), "Set state of qubits in order (ex: 01 or +1 or 0-1)");
 
         bpo::variables_map variables_map;
         bpo::store(bpo::parse_command_line(argc, argv, desc), variables_map);
         bpo::notify(variables_map);
 
-
         if (variables_map.count("qubit")){
-            Flags::handle_q(circuit, variables_map["qubit"].as<char>());
+            Flags::handle_q(circuit, variables_map["qubit"].as<int>());
         }
         
     return 0;
