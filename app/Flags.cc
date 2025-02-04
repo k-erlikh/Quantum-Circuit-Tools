6552251420f312573165eac1dfaee7eb6e886e4b
k-erlikh <kateerlikh@cmail.carleton.ca>
2025-02-04 17:10:17 -0500

    Added new gate delcarations, and updated for smart pointers

--- app/Flags.cc
+++ app/Flags.cc
@@ -1,20 +1,33 @@
 #include "Flags.h"
+#include "PauliX.h"
 
 void Flags::handle_q(Circuit& circuit, int n){
     for(int i = 0; i < n; i++){
-        Qubit* q = new Qubit();
-        circuit.addQubit(q);
+        circuit.addQubit(std::make_unique<Qubit>());
     }
 }
 
-void Flags::handle_s(Circuit& circuit){
+void Flags::handle_s(Circuit& circuit, string &states){
     
 }
 
 void Flags::handle_h(Circuit &circuit, string &qubits){
+    
+}
+
+void Flags::handle_not(Circuit &circuit, string &qubits){
 
 }
 
 void Flags::handle_px(Circuit &circuit, string &qubits){
+    int c = 0, t = -1;
+    circuit.addGate(std::make_unique<PauliX>(c, t));
+}
+
+void Flags::handle_py(Circuit &circuit){
+
+}
+
+void Flags::handle_pz(Circuit &circuit){
 
 }
--- app/Flags.h
+++ app/Flags.h
@@ -4,12 +4,13 @@
 
 class Flags{
     public:
-        static void handle_q(Circuit& circuit, int n); //Qubit
-        static void handle_s(Circuit&); //imulate
-        static void handle_h(Circuit& circuit, string& qubits); //Hadamard Gate
-        static void handle_px(Circuit& circuit, string& qubits); //Pauli X Gate
-        static void handle_py(Circuit& circuit); //Pauli Y Gate
-        static void handle_pz(Circuit& circuit); //Pauli Z Gate
+       static void handle_q(Circuit& circuit, int n); //Qubit
+       static void handle_s(Circuit& circuit, string &states); //imulate
+       static void handle_h(Circuit& circuit, string& qubits); //Hadamard Gate
+       static void handle_not(Circuit &circuit, string&qubits);
+       static void handle_px(Circuit& circuit, string& qubits); //Pauli X Gate
+       static void handle_py(Circuit& circuit); //Pauli Y Gate
+       static void handle_pz(Circuit& circuit); //Pauli Z Gate
 };
 
 #endif
