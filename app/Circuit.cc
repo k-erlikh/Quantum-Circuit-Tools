d4517fe271dd8d9c64b9f944ae3df6fa1ae53759
k-erlikh <kateerlikh@cmail.carleton.ca>
2025-02-04 20:59:01 -0500

    Implemetend print function to view contents of the circuit

--- app/Circuit.cc
+++ app/Circuit.cc
@@ -6,6 +6,25 @@ void Circuit::addGate(unique_ptr<Gate> g){
     this->gates.push_back(std::move(g));
 }
 
-void Circuit::addQubit(unique_ptr<Qubit> q){
-    this->qubits.push_back(std::move(q));
+void Circuit::addQubit(Qubit q){
+    this->qubits.push_back(q);
+}
+
+void Circuit::print_circuit_features(){
+    if(this->qubits.empty()){
+        cout<<"No qubits in circuit"<<endl;
+    }
+    else{
+        for (Qubit q : this->qubits){
+            q.print_state();
+        }
+    }
+    if(this->gates.empty()){
+        cout<<"No gates in circuit"<<endl;
+    }
+    else{
+        for (const auto& g : this->gates){
+            g->gate.print_matrix();
+        }
+    }
 }
--- app/Circuit.h
+++ app/Circuit.h
@@ -10,11 +10,12 @@
 class Circuit{
     public:
         Circuit(int q, int g);
-        void addQubit(unique_ptr<Qubit> q);
+        void addQubit(Qubit q);
         void addGate(unique_ptr<Gate> g);
+        void print_circuit_features();
 
     protected:
-        vector<unique_ptr<Qubit>> qubits;
+        vector<Qubit> qubits;
         vector<unique_ptr<Gate>> gates;
 };
 
